c8857e4ccefb462fd846d181fd6dfc9bf41548a4
andrewstone8 <andrewstone8@gmail.com>
2016-07-14 15:23:33 +0900

    added index.erb and layout.erb content; created helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,25 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
+  get '/' do
+    erb :index
+  end
+
+  helpers do
+    def redirect_if_not_logged_in
+      if !logged_in?
+        redirect "/login?error=You have to be logged in to do that"
+      end
+    end
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
+  end
+
 end
