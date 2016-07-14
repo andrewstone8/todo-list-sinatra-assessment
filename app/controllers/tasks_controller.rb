0646cec30a061c49f0098c676962ffd1d79b74e3
andrewstone8 <andrewstone8@gmail.com>
2016-07-14 18:38:16 +0900

    added tasks controller.rb

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,47 @@
+class ListsController < ApplicationController
+
+  get "/tasks" do
+    redirect_if_not_logged_in 
+    @tasks = Task.all
+    erb :'tasks/index'
+  end
+
+  get "/tasks/new" do
+    redirect_if_not_logged_in 
+    @error_message = params[:error]
+    erb :'tasks/new'
+  end
+
+  get "/tasks/:id/edit" do
+    redirect_if_not_logged_in 
+    @error_message = params[:error]
+    @task = Task.find(params[:id])
+    erb :'tasks/edit'
+  end
+
+  post "/tasks/:id" do
+    redirect_if_not_logged_in 
+    @task = Task.find(params[:id])
+    unless Task.valid_params?(params)
+      redirect "/tasks/#{@task.id}/edit?error=invalid task"
+    end
+    @task.update(params.select{|t|t=="content"})
+    redirect "/tasks/#{@task.id}"
+  end
+
+  get "/tasks/:id" do
+    redirect_if_not_logged_in 
+    @task = Task.find(params[:id])
+    erb :'tasks/show'
+  end
+
+  post "/tasks" do
+    redirect_if_not_logged_in 
+    unless Task.valid_params?(params)
+      redirect "/tasks/new?error=invalid task"
+    end
+    Task.create(params)
+    redirect "/tasks"
+  end
+
+end
